a89f740c03e5e436b25fbef9c890f679083a0a6b
SkylerDooley <skylerdooley@gmail.com>
2026-02-27 14:26:18 -0500

    lots of changes

--- app/README.md
+++ app/README.md
@@ -1,3 +1,6 @@
+Link to get GeoJSON file
+https://eric.clst.org/tech/usgeojson/
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import FarmLineChart from "./components/US_total_farms.js";
+import USFarmMap from "./components/US_farm_map.js";
 
 function App() {
   return (
     <div style={{ padding: 20 }}>
       <h2>Total Farms (Last 5 Years)</h2>
       <FarmLineChart />
+      <USFarmMap />
     </div>
   );
 }
