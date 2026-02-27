2944d6a6f8563310e3ad842e04791cb06443b258
Daniel Mastrobuono <daniel.mastro@comcast.net>
2026-02-27 14:12:28 -0500

    Started the article, slight changes to the line graph.

--- app/src/App.js
+++ app/src/App.js
@@ -3,8 +3,12 @@ import FarmLineChart from "./components/US_total_farms.js";
 
 function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <h2>Total Farms (Last 5 Years)</h2>
+    <div style={{ padding: 20 }} className="app">
+      <h1>The Decline in US Farming in the Last Five Years</h1>
+      <p>For the last few decades, farming has been at a steady decline. In recent history, this has not changed. <br></br>
+        Let's take a look at the numbers over the last 5 years and see which states are most or least impacted.</p>
+      <p>First however, let's take a look at the big picture; the total number of farms in the US.</p>
+      <h2>US Total Farms</h2>
       <FarmLineChart />
     </div>
   );
--- app/src/components/US_total_farms.js
+++ app/src/components/US_total_farms.js
@@ -33,18 +33,21 @@ export default function FarmTotalsLineChart() {
         .range([margin.left, width - margin.right]);
 
       const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.total)])
+        .domain([1850000, 2050000])
         .nice()
         .range([height - margin.bottom, margin.top]);
 
       // axes
       svg.append("g")
         .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+        .call(d3.axisBottom(x).tickValues([2021, 2022, 2023, 2024, 2025]))
+        .call(d3.axisBottom(x).ticks(5).tickFormat(d3.format("d")));
+
 
       svg.append("g")
         .attr("transform", `translate(${margin.left},0)`)
         .call(d3.axisLeft(y).ticks(6).tickFormat(d3.format(",")));
+        
 
       // line generator
       const line = d3.line()
@@ -59,6 +62,18 @@ export default function FarmTotalsLineChart() {
         .attr("stroke-width", 3)
         .attr("d", line);
 
+      //tooltip
+      const tooltip = d3.select("body")
+        .append("div")
+        .style("position", "absolute")
+        .style("background", "white")
+        .style("border", "1px solid #ccc")
+        .style("padding", "6px 10px")
+        .style("border-radius", "6px")
+        .style("font-size", "12px")
+        .style("pointer-events", "none")
+        .style("opacity", 0);
+
       // dots
       svg.selectAll("circle")
         .data(data)
@@ -66,8 +81,30 @@ export default function FarmTotalsLineChart() {
         .append("circle")
         .attr("cx", d => x(d.year))
         .attr("cy", d => y(d.total))
-        .attr("r", 5)
-        .attr("fill", "#1976d2");
+        .attr("r", 4)
+        .attr("fill", "#1976d2")
+
+        // SHOW tooltip
+        .on("mouseover", (event, d) => {
+            tooltip
+            .style("opacity", 1)
+            .html(`
+                <strong>Year:</strong> ${d.year}<br>
+                <strong>Total:</strong> ${d3.format(",")(d.total)}
+            `);
+        })
+
+        // MOVE tooltip
+        .on("mousemove", (event) => {
+            tooltip
+            .style("left", (event.pageX + 12) + "px")
+            .style("top", (event.pageY - 28) + "px");
+        })
+
+        // HIDE tooltip
+        .on("mouseout", () => {
+            tooltip.style("opacity", 0);
+        });
     }
   }, []);
 
